5f274bb322ebcd737eec3d22e38fe2206326bb72
Bozena <86227296+pakalnute@users.noreply.github.com>
2021-10-21 01:17:21 +0300

    DOM uzduotys

---
+++ uzduotys.js
@@ -0,0 +1,40 @@
+// 1.Atskiri elementai
+//     a. Tamsiai žaliai nuspalvinti h1 tagą;
+document.querySelector('h1').style.color = 'darkgreen';
+//     b. Tagui i pridėti klasę small;
+document.querySelector('i').classList.add('small');
+//     c. Iš tago h1 pašalinti klasę main;
+document.querySelector('h1').classList.remove('main');
+//     d. Tik tam h2 tagui, kuris iškart eina po h1 tagui pridėti klasę first ir pašalinti klasę main;
+//     e .Pirmam span tagui, kuris yra h2 viduje sumažinti fonto dydį iki 10px ir nudažyti pilkai
+// 2. Elemetų grupės (nodeList) skaičiavimus išvest į consolę
+//     a. Suskaičiuoti kiek yra h2 tagų;
+//     b. Suskaičiuoti kiek yra h2 tagų, kurie neturi klasės first
+//     c. Visus h2 tagus nuspalvinti šviesiai mėlynai
+//     d. Tagų, kurie yra div su klase prices, viduje esantiems h2 tagams pridėti klasę price-tag;
+//     e. Pabraukti visus tagus su klase new;
+//     f. Suskaičiuoti kiek yra gyvūnų kategorijų ir žirafų (jos yra ul tagai);
+//     g. Tagus ul apibraukite rėmeliais ir uždėkite 15px paddingą viršuje ir apačioje ir 50px paddingą kairėje ir dešinėje;
+//     h. Suskaičiuoti kiek yra naujų gyvūnų (su klase new);
+//     i.Suskaičiuoti atskirai kiek yra naujų gyvūnų kiekvienoje kategorijoje;
+// 3. Elementų events
+//     a. Padaryti tai ką liepia mygtukai Header1 sekcijoje;
+//     b. Padaryti, kad paspaudus ant i tago jis pakeistų fonto svorį į bold;
+//     c. Padaryti, kad paspaudus ant tago su klase prices, backgroundas pasikeistų į pilką, o paspaudus dar kartą vėl grįžtu į baltą spalvą;
+//     d. Padaryti, kad paspaudus ant tago su id contacts, tam tagui būtų pridėta css savybė color = orange;
+//     e. Padaryti taip, kad paspaudus ant padidinti, esančio tage su id contacts, tagui su id contacts būtų pridėta css savybė fontSize = 20px;
+//     f. Padaryti taip, kad paspaudus ant X, esančio tage su id contacts, pridėtos tage su id contacts savybės būtų panaikintos https://stackoverflow.com/questions/18691655/remove-style-on-element
+//     g. Padaryti tai ką liepia mygtukai Header2 sekcijoje;
+// 4. Elementų grupių events
+//     a. Padaryti, kad dukartus paspaudus ant naujų gyvūnų jie nusispalvintu raudonai https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+//     b. Padaryti, kad paspaudus ant gyvūno jis būtų atvaizduojamas 130% didesniu fonto dydžiu. “PATINKA” tas neturi galioti.
+//     c. Padaryti, kad paspaudus ant “PATINKA”, atitinkamai sekcijai būtų priskirta klasė like;
+// 5. Dinaminis elementų kūrimas (su createElement)
+//     a. Dinamiškai su JS pridėti naują kainą “Senjorai tik: 1.99 eur”;
+//     b. Dinamiškai su JS Pridėti naują kainą “Senjorų grupė iki 10: tik 5.99 eur” Padaryti, kad pridėtas elementas turėtų klasę new ir ant jo paklikinus jis pasidarytų žalias;
+//     c. Dinamiškai su JS kiekvienoje gyvūnų kategorijoje po “PATINKA” pridėkite dar vieną li elementą “NEPATINKA”, kurį paspaudus atitinkamoje sekcijoje būtų nuimta klasė like
+//     d. Dinamiškai su JS sukurkite naują mygtukų grupę HEADER 3 naudojant analogišką html tagų struktūrą kaip ir HEADER 1 ir HEADER 2. Pirmas mygtukas vadintųsi, “Pabraukti H1 tagą”, o antras “Nepabraukti H1 tagą”. Mygtukai turi daryti tai kas ant jų parašyta
+
+
+
+
